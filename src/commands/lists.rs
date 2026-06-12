7a1f852a59b0b5a6bb78987f58fd45a7a9d6b174
maeve <maeve@oa.ke>
2026-06-12 14:08:06 +0100

    accidental comment

--- src/commands/lists.rs
+++ src/commands/lists.rs
@@ -121,55 +121,55 @@ sp_coop             -Singleplayer Coop```"
 
 pub fn playlistvars(_options: &[CommandDataOption]) -> String {
     "```Diff
-// + Playlist Vars
-// custom_air_accel_pilot
-// pilot_health_multiplier
-// run_epilogue
-// respawn_delay
-// boosts_enabled
-// earn_meter_pilot_overdrive
-// earn_meter_pilot_multiplier
-// earn_meter_titan_multiplier
-// aegis_upgrades
-// infinite_doomed_state
-// titan_shield_regen
-// scorelimit
-// roundscorelimit
-// timelimit
-// oob_timer_enabled
-// roundtimelimit
-// classic_rodeo
-// classic_mp
-// fp_embark_enabled
-// promode_enable
-// riff_floorislava
-// featured_mode_all_holopilot
-// featured_mode_all_grapple
-// featured_mode_all_phase
-// featured_mode_all_ticks
-// featured_mode_tactikill
-// featured_mode_amped_tacticals
-// featured_mode_rocket_arena
-// featured_mode_shotguns_snipers
-// iron_rules
-// riff_player_bleedout
-// player_bleedout_forceHolster
-// player_bleedout_forceDeathOnTeamBleedout
-// player_bleedout_bleedoutTime
-// player_bleedout_firstAidTime
-// player_bleedout_firstAidTimeSelf
-// player_bleedout_firstAidHealPercent
-// player_bleedout_aiBleedingPlayerMissChance
++ Playlist Vars
+custom_air_accel_pilot
+pilot_health_multiplier
+run_epilogue
+respawn_delay
+boosts_enabled
+earn_meter_pilot_overdrive
+earn_meter_pilot_multiplier
+earn_meter_titan_multiplier
+aegis_upgrades
+infinite_doomed_state
+titan_shield_regen
+scorelimit
+roundscorelimit
+timelimit
+oob_timer_enabled
+roundtimelimit
+classic_rodeo
+classic_mp
+fp_embark_enabled
+promode_enable
+riff_floorislava
+featured_mode_all_holopilot
+featured_mode_all_grapple
+featured_mode_all_phase
+featured_mode_all_ticks
+featured_mode_tactikill
+featured_mode_amped_tacticals
+featured_mode_rocket_arena
+featured_mode_shotguns_snipers
+iron_rules
+riff_player_bleedout
+player_bleedout_forceHolster
+player_bleedout_forceDeathOnTeamBleedout
+player_bleedout_bleedoutTime
+player_bleedout_firstAidTime
+player_bleedout_firstAidTimeSelf
+player_bleedout_firstAidHealPercent
+player_bleedout_aiBleedingPlayerMissChance
 
-// + Sticks and Stones Playlist Vars
-// sns_softball_enabled
-// sns_softball_kill_value
-// sns_wme_kill_value
-// sns_offhand_kill_value
-// sns_reset_kill_value
-// sns_melee_kill_value
-// sns_reset_pulse_blade_cooldown_on_pulse_blade_kill
-// ```"
++ Sticks and Stones Playlist Vars
+sns_softball_enabled
+sns_softball_kill_value
+sns_wme_kill_value
+sns_offhand_kill_value
+sns_reset_kill_value
+sns_melee_kill_value
+sns_reset_pulse_blade_cooldown_on_pulse_blade_kill
+```"
         .to_string()
 }
 
